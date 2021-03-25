2aef4aca1fd50cf12fb0986cb9795b29af829dd6
Mohamed Sharaf Jr <mohamedsharafjr@gmail.com>
2021-03-25 15:45:45 +0200

    fix indentation

--- src/app/store/hooks/useCategory.tsx
+++ src/app/store/hooks/useCategory.tsx
@@ -1,11 +1,11 @@
 import { Category } from '../types/category';
 
-export const useCategory = ( id: number ): Category => {
-  const dummy = {
-    id: 1,
-    name: 'category-1',
-    slug: 'slug-1',
-  };
+export const useCategory = (id: number): Category => {
+    const dummy = {
+        id: 1,
+        name: 'category-1',
+        slug: 'slug-1',
+    };
 
-  return dummy;
+    return dummy;
 }
