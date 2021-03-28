c149125f07fcaa0a194dcdedf80b71b3f61fa9fd
Mohamed Sharaf Jr <mohamedsharafjr@gmail.com>
2021-03-28 19:50:01 +0200

    update ArticleGrid component

--- src/app/components/articles-grid.tsx
+++ src/app/components/articles-grid.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { usePostQuery } from '../store/hooks/usePostQuery';
 import Article from './article';
 
-const ArticlesGrid = (props: any) => {
-    if ( ! props.articles.length ) {
+const ArticlesGrid = () => {
+    const postQuery = usePostQuery({ page: 0, per_page: 10 });
+
+    if ( postQuery.status === 'pending' ) {
         return <em>Loading</em>;
+    }
+
+    if ( postQuery.status === 'failed' ) {
+        return <em>try again later</em>;
+    }
+
+    if ( postQuery.status === 'finished' && ! postQuery.result.posts.length ) {
+        return <em>no articles yet</em>;
     }
 
     return (
         <div>
-            {props.articles.map((post: any, i: number) => (
+            {postQuery.result.posts.map((post: any, i: number) => (
                 <Article key={i} postId={post.id} />
             ))}
         </div>
