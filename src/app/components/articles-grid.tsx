adae8a9393d2bc2a81cc8e17e39e63bcc6f98c4c
Mohamed Sharaf Jr <mohamedsharafjr@gmail.com>
2021-03-29 13:56:24 +0200

    use elseif block

--- src/app/components/articles-grid.tsx
+++ src/app/components/articles-grid.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import { usePostQuery } from '../store/hooks/usePostQuery';
+import { Post } from '../store/types/post';
 import Article from './article';
 
 const ArticlesGrid = () => {
-    const postQuery = usePostQuery({ page: 0, per_page: 10 });
+    const postQuery = usePostQuery({ page: 1, per_page: 10 });
 
     if ( postQuery.status === 'pending' ) {
-        return <em>Loading</em>;
+        return (
+            <em>Loading</em>
+        );
+    } else if ( postQuery.status === 'failed' ) {
+        return (
+            <em>try again later</em>
+        );
+    } else if ( postQuery.status === 'finished' && ! postQuery.result.posts.length ) {
+        return (
+            <em>no articles yet</em>
+        );
+    } else {
+        return (
+            <div>
+                {postQuery.result.posts.map((post: Post, i: number) => (
+                    <Article key={i} id={post.id} />
+                ))}
+            </div>
+        );
     }
-
-    if ( postQuery.status === 'failed' ) {
-        return <em>try again later</em>;
-    }
-
-    if ( postQuery.status === 'finished' && ! postQuery.result.posts.length ) {
-        return <em>no articles yet</em>;
-    }
-
-    return (
-        <div>
-            {postQuery.result.posts.map((post: any, i: number) => (
-                <Article key={i} postId={post.id} />
-            ))}
-        </div>
-    );
 }
 
 export default ArticlesGrid;
