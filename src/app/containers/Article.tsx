0e370c2cded41bd29cb04fbc16425b57751e481c
Mohamed Sharaf Jr <mohamedsharafjr@gmail.com>
2021-03-30 15:45:40 +0200

    add classnames

--- src/app/containers/Article.tsx
+++ src/app/containers/Article.tsx
@@ -3,6 +3,9 @@ import { useParams } from 'react-router';
 import Author from '../components/author';
 import Thumbnail from '../components/thumbnail';
 import { usePostBySlug } from '../store/hooks/usePostBySlug';
+import classnames from 'classnames-prefix';
+
+const cx = classnames( 'article' );
 
 export default function Article() {
     const { slug }: { slug: string } = useParams();
@@ -10,16 +13,16 @@ export default function Article() {
 
     return (
         <main>
-            <article>
-                <h1>{title.rendered}</h1>
-                <div>
+            <article className={cx('')}>
+                <h1 className={cx('__title')}>{title.rendered}</h1>
+                <div className={cx('__thumbnail')}>
                     <Thumbnail id={meta._thumbnail_id} />
                 </div>
-                <div>
+                <div className={cx('__info')}>
                     <span>By</span>
                     <Author id={author} />
                 </div>
-                <div>
+                <div className={cx('__content')}>
                     {content.rendered}
                 </div>
             </article>
