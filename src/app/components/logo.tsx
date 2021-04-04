eae596250c41d67e3e839b0368af4a6b011e176b
Mohamed Sharaf Jr <mohamedsharafjr@gmail.com>
2021-04-04 13:05:32 +0200

    add randoms logo

--- src/app/components/logo.tsx
+++ src/app/components/logo.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo.jpg';
 
-const Logo = () => (
-    <Link className='site-logo' to='/'>
-        <img
-            src={logo}
-            alt='3shwa2yat site logo'
-            height='40'
-            width='40'
-        />
-    </Link>
-);
-
-export default Logo;
+export default function Logo () {
+    return (
+        <Link className='site-logo' to='/'>
+            <h1>
+                <img
+                    title='الموقع الرسمي لمجموعة قنوات عشوائيات'
+                    src={logo}
+                    alt='عشوائيات'
+                    height='46'
+                    width='46'
+                />
+            </h1>
+        </Link>
+    );
+}
