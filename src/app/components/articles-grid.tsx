9328186eb3cdb74f08ed9693014e72e7a50db93b
Mohamed Sharaf Jr <mohamedsharafjr@gmail.com>
2021-04-05 14:19:42 +0200

    add divider component

--- src/app/components/articles-grid.tsx
+++ src/app/components/articles-grid.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { usePostQuery } from '../store/hooks/usePostQuery';
 import { Post } from '../store/types/post';
 import Article from './article';
+import Divider from './divider'
 
 const ArticlesGrid = () => {
     const postQuery = usePostQuery({ page: 1, per_page: 10 });
@@ -22,9 +23,7 @@ const ArticlesGrid = () => {
         return (
             <div className='articles-grid'>
                 <div className='container'>
-                    <div className='divider'>
-                        <span>احدث المقالات</span>
-                    </div>
+                    <Divider>احدث المقالات</Divider>
                     <div className='posts'>
                         {postQuery.result.posts.map((post: Post, i: number) => (
                             <Article key={i} id={post.id} />
