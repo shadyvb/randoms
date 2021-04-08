45cbec534f372e7052d7632509564cc49b9dc2bd
Mohamed Sharaf Jr <mohamedsharafjr@gmail.com>
2021-04-08 16:46:28 +0200

    pass boolean flags

--- src/app/components/articles-grid.tsx
+++ src/app/components/articles-grid.tsx
@@ -7,32 +7,40 @@ import Divider from './divider'
 const ArticlesGrid = () => {
     const postQuery = usePostQuery({ page: 1, per_page: 10 });
 
-    if ( postQuery.status === 'pending' ) {
+    if ( postQuery.isPending ) {
         return (
             <em>Loading</em>
         );
-    } else if ( postQuery.status === 'failed' ) {
+    }
+
+    if ( postQuery.isFailed ) {
         return (
             <em>try again later</em>
         );
-    } else if ( postQuery.status === 'finished' && ! postQuery.result.posts.length ) {
+    }
+
+    if (
+        postQuery.isFinished &&
+        ! postQuery.isFailed &&
+        ! postQuery.result.posts.length
+    ) {
         return (
             <em>no articles yet</em>
         );
-    } else {
-        return (
-            <div className='articles-grid'>
-                <div className='container'>
-                    <Divider>احدث المقالات</Divider>
-                    <div className='posts'>
-                        {postQuery.result.posts.map((post: Post, i: number) => (
-                            <Article key={i} id={post.id} />
-                        ))}
-                    </div>
+    }
+
+    return (
+        <div className='articles-grid'>
+            <div className='container'>
+                <Divider>احدث المقالات</Divider>
+                <div className='posts'>
+                    {postQuery.result.posts.map((post: Post, i: number) => (
+                        <Article key={i} id={post.id} />
+                    ))}
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default ArticlesGrid;
