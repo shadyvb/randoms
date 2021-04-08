d507cbe4299f0408da0be7ada4f67ae807f28856
Mohamed Sharaf Jr <mohamedsharafjr@gmail.com>
2021-04-08 15:41:26 +0200

    SidebarMenu -> NavMenuTrigger

---
+++ src/app/components/nav-menu-trigger.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function NavMenuTrigger() {
+    return (
+        <div className='nav-menu-trigger'>
+            <label htmlFor='nav-trigger'>
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+            </label>
+        </div>
+    );
+}
--- src/app/components/sidebar-trigger.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SidebarTrigger = () => (
-    <div className='sidebar-trigger'>+</div>
-);
-
-export default SidebarTrigger;
