e80f58e4e62b6b0c1c398107c50548ce5d7752f3
Mohamed Sharaf Jr <mohamedsharafjr@gmail.com>
2021-04-08 15:43:09 +0200

    add checkbox input as a nav-menu trigger

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import NavMenu from './nav-menu';
 import SocilLinks from './social-links';
-import SidebarTrigger from './sidebar-trigger';
+import NavMenuTrigger from './nav-menu-trigger';
 import Logo from './logo';
 
 const Header = () => {
     return (
         <header className='header'>
+            <input
+                type='checkbox'
+                id='nav-trigger'
+                className='visually-hidden'
+                aria-hidden={true}
+                hidden={true}
+            />
             <div className='header__container container'>
-                <SidebarTrigger />
                 <Logo />
                 <NavMenu />
                 <SocilLinks />
+                <NavMenuTrigger />
             </div>
         </header>
     );
